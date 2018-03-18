f322a320bc5bb6f1ebb44a365b4a40bab585a55d
Olexandr Dubrov <olexandr.dubrov@gmail.com>
2018-03-18 21:40:30 +0200

    refactor Products and fix some bugs

--- src/shopping_list.py
+++ src/shopping_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 __version__ = '1.0b'
 __author__ = 'OD'
 
@@ -10,9 +9,9 @@ SYMBIAN = True if sys.platform == 'symbian_s60' else False
 
 if SYMBIAN:
 	import e32  # @UnresolvedImport
-	import appuifw  # @UnresolvedImport
+	import appuifw  # @UnresolvedImport @UnusedImport
 else:
-	import symbian.appuifw as appuifw
+	import symbian.appuifw as appuifw  # @Reimport
 
 EMPTY_LIST_MARK = unicode('The shopping list is empty.')
 
@@ -64,127 +63,116 @@ class Products:
 	
 	def __init__(self, xml_file):
 		self._xml_file = xml_file
-		self._departs, self._products = self._getData(xml_file)
-		print self._products
+		self._departs = self._getData()
 		self.last_msg = u''
-		
-	def _getData(self, xml_file):
+	
+	def _getData(self):
 		# Get a list of products.
 		# Each product is a dict with a name and a flag (is in list)
-		self.tree = ET.parse(xml_file)
-		self.root = self.tree.getroot()
-		assert self.root.tag == 'products', 'unknown xml'
-		products = [] #it is going to be a list of dictionaries
+		tree = ET.parse(self._xml_file)
+		root = tree.getroot()
+		assert root.tag == 'products', 'unknown xml'
 		departs = []
-		for depart in self.root:
-			departs.append(depart.attrib)
+		for depart in root:
+			products = []
 			for prod in depart:
-				products.append(prod.attrib)
-		return departs, products
+				products.append({'name': prod.attrib['name'], 'chk': prod.attrib['inlist']})
+			departs.append({'name': depart.attrib['name'], 'products': products})
+		return departs
 	
 	def saveData(self):
-		for depart in self.root:
-			for prod in depart:
-				for p in self._products:
-					if prod.attrib['name'] == p['name']:
-						prod.attrib['inlist'] = p['inlist']
-						break
-		self.tree.write(self._xml_file)
-		
-	@property
-	def products(self):
-		return self._products
+		root = ET.Element('products')
+		for dep in self._departs:
+			d = ET.SubElement(root, 'department', {'name': dep['name']})
+			for prod in dep['products']:
+				ET.SubElement(d, 'product', {'inlist': prod['chk'], 'name': prod['name']})
+		tree = ET.ElementTree(root)
+		tree.write(self._xml_file)
 	
-	@property
 	def departments(self):
-		return self._departs
+		return [dep['name'] for dep in self._departs]
 	
-	def putInList(self, name):
-		self._setInlistVal(name, '1')
+	def put_in_list(self, name):
+		self._set_inlist_val(name, '1')
 				
-	def putOutList(self, name):
-		self._setInlistVal(name, '0')
+	def put_out_list(self, name):
+		self._set_inlist_val(name, '0')
 		
-	def _setInlistVal(self, name, value):
-		def cb_setter(item):
-			if item['name'] == name:
-				item['inlist'] = value
-			return item
-		self._products = map(cb_setter, self._products)
+	def _set_inlist_val(self, name, value):
+		for deps in self._departs:
+			for prod in deps['products']:
+				if prod['name'] == name:
+					prod['chk'] = value
+					return
 	
-	def getAllProducts(self):
-		return [unicode(x['name']) for x in self._products]
+	def is_product_checked(self, product):
+		return product['chk'] == '1'
+	
+	def _products(self):
+		return [prod for dep in self._departs for prod in dep['products']]
+	
+	def get_all_products(self):
+		return [unicode(prod['name']) for dep in self._departs for prod in dep['products']]
 		
-	def getChecked(self):
-		#return a list of names that are checked
-		return [unicode(x['name']) for x in self._products if x['inlist'] == '1']
+	def get_checked(self):
+		# return a list of names that are checked
+		return [unicode(x['name']) for x in self._products() if self.is_product_checked(x)]
 	
-	def getUnchecked(self):
+	def get_unchecked(self):
 		#return a list of names that are NOT checked
-		return [unicode(x['name']) for x in self._products if x['inlist'] == '0']
+		return [unicode(x['name']) for x in self._products() if not self.is_product_checked(x)]
 	
-	def getDepartsList(self):
+	def get_departs_list(self):
 		return [unicode(x['name']) for x in self._departs]
 	
-	def getCheckedByDep(self, name):
+	def get_checked_by_dep(self, name):
 		if not name:
-			return self.getChecked()
-		
-		for depart in self.root:
-			if depart.attrib['name'] == name:
-				return [prod.attrib['name'] + ': ' + name for prod in depart if prod.attrib['inlist'] == '1']
+			return self.get_checked()
+		for depart in self._departs:
+			if depart['name'] == name:
+				return [prod['name'] + ': ' + name for prod in depart if self.is_product_checked(prod)]
 		return []
 	
-	def addProduct(self, name, depart_name):
+	def add_product(self, name, depart_name):
 		#check if the product has already been in the list
-		if unicode(name) in [unicode(x['name']) for x in self._products]:
+		if name in [x for x in self.get_all_products()]:
 			self.last_msg = u'The product has already been in the list.'
 			return 1
-		#update the product list
-		product = {'name': name, 'inlist': '1'}
-		self._products.append(product)
-		#update the xml tree
-		for depart in self.root:
-			if depart.attrib['name'] == depart_name:
-				ET.SubElement(depart, 'product', product)
-				return 0
-		self.last_msg = u'%s is not found'%depart_name
-		return 1
+		else:
+			#update the product list
+			product = {'name': name, 'chk': '1'}
+			for dep in self._departs:
+				if dep['name'] == depart_name:
+					dep['products'].append(product)
+					return 0
+			self.last_msg = u'%s not found.'%(depart_name)
+			return 1
 	
 	def removeProduct(self, name):
-		for x in self._products:
-			if x['name'] == name:
-				#udate the tree
-				for depart in self.root:
-					for prod in depart:
-						if prod.attrib['name'] == name:
-							depart.remove(prod)
-							#update the list
-							self._products.remove(x)
-							return 0
+		for idx in range(len(self._departs)):
+			for jdx in range(len(self._departs[idx]['products'])):
+				if name == self._departs[idx]['products'][jdx]['name']:
+					del self._departs[idx]['products'][jdx]
+					return 0
 		self.last_msg = u'The product is not in the list.'
 		return 1
 	
 	def addDepart(self, name):
-		for depart in self.root:
-			if depart.attrib['name'] == name:
-				self.last_msg = u'A department with name %s has been in the list'%name
-				return 1
-		new_dep = {'name': name}
-		ET.SubElement(self.root, 'department', new_dep)
+		if name in [x['name'] for x in self._departs]:
+			self.last_msg = u'A department with name %s has been in the list'%name
+			return 1
+		new_dep = {'name': name, 'products': []}
 		self._departs.append(new_dep)
 		return 0
 	
 	def removeDepart(self, name):
-		for x in self._departs:
-			if x['name'] == name:
-				for depart in self.root:
-					if depart.attrib['name'] == name:
-						self.root.remove(depart)
-						#udate the list
-						self._departs.remove(x)
-						return 0		
-		self.last_msg = u'The department is not in the list'
+		if name not in [x['name'] for x in self._departs]:
+			self.last_msg = u'A department with name %s is not in the list'%name
+			return 1
+		for idx in range(len(self._departs)):
+			if self._departs[idx]['name'] == name:
+				del self._departs[idx]
+				return 0
 		return 1
 
 
@@ -193,17 +181,17 @@ class ShoppingList:
 	
 	TITLE = u"Shopping list"
 	if SYMBIAN:
-		XML_DATA_FILE = 'e:\\data\\python\\products.xml'
+		JSON_DATA_FILE = 'e:\\data\\python\\products.xml'
 	else:
-		XML_DATA_FILE = '../db/products.xml'
+		JSON_DATA_FILE = '../db/products.xml'
 	
 	def __init__(self):
 		appuifw.app.title = ShoppingList.TITLE
 		appuifw.app.screen = "normal"
 		
-		self.products = Products(ShoppingList.XML_DATA_FILE)
+		self.products = Products(ShoppingList.JSON_DATA_FILE)
 		
-		self.products_list = Listbox(self.products.getChecked(), self.products_list_handler)
+		self.products_list = Listbox(self.products.get_checked(), self.products_list_handler)
 		appuifw.app.body = self.products_list.ui_list
 		self.products_list.ui_list.bind(0x35, self.products_list_handler) #bind the handler to key 5
 		self.products_list.ui_list.bind(0x32, self.products_list.cb_focus_up) #bind the handler to key 2
@@ -246,12 +234,12 @@ class ShoppingList:
 		if self.product_mode:
 			appuifw.app.title = u"Shopping list. Departments." 
 			appuifw.app.menu[1] = (u'All products', self.at_mode)
-			self.products_list.set_list([u'-- All departments',] + self.products.getDepartsList())
+			self.products_list.set_list([u'-- All departments',] + self.products.get_departs_list())
 			self.product_mode = False
 		else:
 			appuifw.app.title = ShoppingList.TITLE
 			appuifw.app.menu[1] = (u'By departments', self.at_mode)
-			self.products_list.set_list(self.products.getChecked())
+			self.products_list.set_list(self.products.get_checked())
 			self.product_mode = True
 		
 	def _removeDepName(self, full_name):
@@ -268,26 +256,27 @@ class ShoppingList:
 		if self.product_mode:
 			#remove department name
 			if name != EMPTY_LIST_MARK:
-				self.products.putOutList(self._removeDepName(name))
+				self.products.put_out_list(self._removeDepName(name))
 				self.products_list.remove_item(name)
 		else:
-			lst = self.products.getCheckedByDep(name if not name.startswith(u'--') else None)
+			lst = self.products.get_checked_by_dep(name if not name.startswith(u'--') else None)
 			self.products_list.set_list(lst if len(lst) else [EMPTY_LIST_MARK,])
 			self.product_mode = True
+			appuifw.app.title = u"Shopping list. %.", name
 		
 	def run(self):
 		pass
 	
 	def at_home(self):
 		appuifw.app.title = u"At home. Select a product to buy."
-		unchecked = self.products.getUnchecked()
+		unchecked = self.products.get_unchecked()
 		if len(unchecked) == 0:
 			appuifw.note(u"Everything has been added to the shopping list.", "info")
 		else:
 			idxs = appuifw.multi_selection_list(unchecked, style='checkbox', search_field=1)
 			for idx in idxs:
-				self.products.putInList(unchecked[idx])
-			self.products_list.set_list(self.products.getChecked())
+				self.products.put_in_list(unchecked[idx])
+			self.products_list.set_list(self.products.get_checked())
 
 		#switch mode to product
 		self.product_mode = False
@@ -295,20 +284,20 @@ class ShoppingList:
 	
 	def at_addProduct(self):
 		appuifw.app.title = u'Add a new product. Select a department.'
-		departs = self.products.getDepartsList()
+		departs = self.products.get_departs_list()
 		idx = appuifw.selection_list(departs, search_field=1)
 		if not idx == None:
 			depart_name = departs[idx]
 			appuifw.app.title = u'Add a new product.'
 			prod_name = appuifw.query(u'Enter a product name.', 'text')
 			if prod_name and not u':' in prod_name: # : used to separate product name and department in 'by depart' mode
-				if not prod_name.lower() in self.products.getAllProducts():
-					if self.products.addProduct(prod_name.lower(), depart_name):#return 1 - cannot add, otherwise - 0
+				if not prod_name.lower() in self.products.get_all_products():
+					if self.products.add_product(prod_name.lower(), depart_name):#return 1 - cannot add, otherwise - 0
 						appuifw.note(u"Cannot add the product.\n%s"%self.products.last_msg, "error")
 					else:
 						self.products_list.add_item(prod_name.lower())
 				else:
-					appuifw.note(u'%d has been at the list.'%prod_name.lower())
+					appuifw.note(u'%s has been at the list.'%prod_name.lower(), "error")
 			else:
 				pass # 'cancel' was presssed on product name
 		else:
@@ -317,7 +306,7 @@ class ShoppingList:
 		
 	def at_removeProduct(self):
 		appuifw.app.title = u'Remove a new product. Select a department.'
-		lst = self.products.getAllProducts()
+		lst = self.products.get_all_products()
 		idx = appuifw.selection_list(lst, search_field=1)
 		if not idx == None:
 			prod_name = lst[idx]
@@ -331,12 +320,13 @@ class ShoppingList:
 		depart_name = appuifw.query(u'Add a depart name.', 'text')
 		if depart_name:
 			if self.products.addDepart(depart_name):
-				appuifw.note(u'%s'%self.last_msg, 'error')
+				appuifw.note(u'%s'%self.products.last_msg, 'error')
 		appuifw.app.title = ShoppingList.TITLE
+		appuifw.note(u'%s added.'%depart_name, 'info')
 	
 	def at_removeDepart(self):
 		appuifw.app.title = u'Remove a department.'
-		lst = self.products.getDepartsList()
+		lst = self.products.get_departs_list()
 		idx = appuifw.selection_list(lst, search_field=1)
 		if not idx == None:
 			depart_name = lst[idx]
@@ -352,7 +342,8 @@ class ShoppingList:
 	def quit(self):
 		self.products.saveData()
 		appuifw.app.exit_key_handler = None
-		self.script_lock.signal()
+		if SYMBIAN:
+			self.script_lock.signal()
 
 
 if __name__ == '__main__':
--- tests/shopping_list_tester.py
+++ tests/shopping_list_tester.py
@@ -13,8 +13,13 @@ class Shopping_list_tester(unittest.TestCase):
             return gen.next()
         __builtin__.raw_input = raw
     
+    def resetXml(self):
+        with open(ShoppingList.JSON_DATA_FILE, 'w') as f:
+            f.write('<products></products>')
+        
     def setUp(self):
-        ShoppingList.XML_DATA_FILE = '../db/test_products.xml'
+        ShoppingList.JSON_DATA_FILE = '../db/test_products.xml'
+        self.resetXml()
         self.sut = ShoppingList()
         self.mock_raw_input(['dep1',
                              0, 'prod11',
@@ -33,13 +38,13 @@ class Shopping_list_tester(unittest.TestCase):
         
     def test_test(self):
         print self.sut.products_list.lst
-        print self.sut.products.getAllProducts()
+        print self.sut.products.get_all_products()
         print self.sut.products.departments
-        print self.sut.products.getChecked()
-        print self.sut.products.getUnchecked()
-        print self.sut.products.getDepartsList()
+        print self.sut.products.get_checked()
+        print self.sut.products.get_unchecked()
+        print self.sut.products.get_departs_list()
     
-    def test_show_all_after_deparm(self):
+    def test_show_all_after_depart(self):
         self.sut.at_mode()
         self.failIf(appuifw.app.title != u"Shopping list. Departments.")
         self.failIf(self.sut.products_list.lst[0] != u'-- All departments')
@@ -47,6 +52,30 @@ class Shopping_list_tester(unittest.TestCase):
         self.sut.at_mode()
         self.assertEqual(appuifw.app.title, u"Shopping list")
         self.assertEqual([u'prod11', u'prod12', u'prod21', u'prod22'],
-                         self.sut.products.getChecked())
-        
-        
+                         self.sut.products.get_checked())
+    
+    def  test_add_the_product_twice(self):
+        ''' add the stdout verification if you want '''
+        self.mock_raw_input([0, 'prod11'])
+        self.sut.at_addProduct()
+    
+    def test_remove_product(self):
+        self.mock_raw_input(['2', 'prod21'])
+        self.sut.at_removeProduct()
+        self.assertEqual([u'prod11', u'prod12', u'prod22'],
+                         self.sut.products.get_checked())
+    
+    def test_remove_depart_with_prod(self):
+        self.mock_raw_input([0, '1', 'prod21'])
+        self.sut.at_removeDepart()
+        self.assertEqual([u'prod21', u'prod22'],
+                         self.sut.products.get_checked())
+    
+    def test_save_data(self):
+        self.sut.quit()
+    
+    def test_put_in_out_list(self):
+        self.sut.products.put_out_list('prod22')
+        self.sut.products.put_in_list('prod22')
+        self.assertEqual([u'prod11', u'prod12', u'prod21', u'prod22'],
+                         self.sut.products.get_checked())
